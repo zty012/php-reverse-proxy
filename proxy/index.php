8680e0f55c93725420bf4806888bdc7ccae5b136
shikukuya <shikukuya@qq.com>
2023-06-17 14:44:01 +0800

    🎉 init:

--- proxy/index.php
+++ proxy/index.php
@@ -4,18 +4,27 @@
  * v1.0 By shikukuya
  */
 
+$url_pattern = "/https?:\/\/[-A-Za-z0-9+&@#\/%?=~_|!:,.;]+[-A-Za-z0-9+&@#\/%=~_|]/";
 $real_url = $_SERVER["QUERY_STRING"];
 $server_name = $_SERVER["SERVER_NAME"];
 $method = $_SERVER["REQUEST_METHOD"];
 $headers = getallheaders();
-if (!preg_match("/https?:\/\/.+/", $real_url)) {
-    $real_url = "http://" . $real_url;
+$headers_string = "";
+foreach ($headers as $k => $v) {
+    $headers_string = "$headers_string$k: $v\n";
+}
+if (!preg_match($url_pattern, $real_url)) {
+    $real_url = "http://$real_url";
 }
 $options["http"] = array(
     "timeout" => 15,
     "method" => $method,
-    "header" => $headers,
+    "header" => $headers_string,
 );
 $context = stream_context_create($options);
 $response = file_get_contents($real_url, false, $context);
+$response = preg_replace_callback($url_pattern, function ($m) {
+    global $server_name;
+    return "http://$server_name/proxy/" . $m[0];
+}, $response);
 echo $response;
